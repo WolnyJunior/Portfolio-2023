8579f69ecb4116bc0d030833c8dd6bc36f2f57f6
SorinJunior <juninhosorin@gmail.com>
2023-05-10 20:00:29 -0300

    commit-botao-top

--- Script/script.js
+++ Script/script.js
@@ -18,3 +18,25 @@ var cards = document.querySelectorAll('.cartao');
         cartao.classList.toggle('flip');
     });
 });
+
+
+function goToTop() {
+    document.querySelector('.top').scrollIntoView({ behavior: 'smooth' });
+  
+    let isScrolling;
+  
+    const handleAutoScrollEnd = () => {
+        window.clearTimeout(isScrolling);
+  
+        isScrolling = setTimeout(function() {
+            document.querySelector('.go-to-top-btn').classList.add('invisible')
+            window.removeEventListener('scroll', handleAutoScrollEnd);
+        }, 66);
+    };
+  
+    window.addEventListener('scroll', handleAutoScrollEnd, false);
+  }
+  
+  document.addEventListener('scroll', () => {
+    document.querySelector('.go-to-top-btn').classList.remove('invisible');
+  })
